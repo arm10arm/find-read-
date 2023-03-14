aae677b717e841740066579f9d1d1b4afc483146
arm10arm <arm10armarm@gmail.com>
2023-03-14 18:46:23 +0700

    add vue

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+
+<template class="">
+  <div class="text-center font-medium text-5xl">Shop list</div>
+  <hr class="bg-stone-800 h-1 mx-auto my-4 w-1/2">
+  <div class=" grid grid-cols-3 gap-5 p-4 text-slate-100 text-center">
+
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >SE-ED</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >NAIIN</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >KINOKUNIYA</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >ASIA-BOOKS</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >B2S</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >KLINGNANGSUE</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >WELEARN</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >AMARIN</a></div>
+    <div class="bg-stone-700 rounded-md p-4"><a href="#" >SPRINGBOOKS</a></div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      title: 'My To-Do App',
+      newTask: '',
+      tasks: [
+        { id: 1, name: 'Learn VUE js', finished: false },
+        { id: 2, name: 'Build a Vue App', finished: false },
+        { id: 3, name: 'Build Todo App', finished: false }
+      ]
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.length < 1) return; 
+
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        name: this.newTask,
+        finished: false
+      });
+
+      this.newTask = '';
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/intro.vue
@@ -0,0 +1,36 @@
+<template class = "bg-zinc-900">
+    <div class="flex items-center justify-center"  style="height: 100vh; width: 100%;">
+        <div class="container text-center w-64">
+            <p class="title text-5xl text-neutral-800	">
+                Find-read
+            </p>
+            <p class="subtitle text-neutral-800	">
+                Let's see what do you want
+            </p>
+            <button class="w-20 h-7 bg-slate-600	adjust rounded-lg"><a  class ="text-neutral-800" href="index.html">Let's see</a></button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+ *{
+        font-family: 'Gloock', serif;
+      }
+      .title:hover, .subtitle:hover{
+        color: whitesmoke;
+        padding: 0.5rem;
+        text-align: center;
+        text-decoration: none;
+        transition: all 0.5s ease-out;
+      }
+      .adjust:hover{
+        padding: 0.5rem;
+        text-align: center;
+        text-decoration: none;
+        transition: all 0.5s ease-out;
+      }
+</style>
