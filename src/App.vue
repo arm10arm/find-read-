c2239d8484297d7b3e9fa044b1f06d1ad50a9f4e
arm10arm <arm10armarm@gmail.com>
2023-03-15 17:17:33 +0700

    chanfg to vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
-<template class = "">
-  <div class="flex items-center justify-center bg-zinc-900"  style="height: 100vh; width: 100%;">
-      <div class="container text-center w-64">
-          <p class="title text-5xl text-neutral-800	">
-              Find-read
-          </p>
-          <p class="subtitle text-neutral-800	">
-              Let's see what do you want
-          </p>
-          <button class="w-20 h-7 bg-slate-600	adjust rounded-lg"><a  class ="text-neutral-800" href="home.vue">Let's see</a></button>
-      </div>
-  </div>
+<template lang="">
+  <router-view></router-view>
 </template>
+<style lang="">
 
-<script>
-export default {
-    
-}
-</script>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Gloock&family=Mitr:wght@300&display=swap');
-*{
-      font-family: 'Gloock', serif;
-    }
-    .title:hover, .subtitle:hover{
-      color: whitesmoke;
-      padding: 0.5rem;
-      text-align: center;
-      text-decoration: none;
-      transition: all 0.5s ease-out;
-    }
-    .adjust:hover{
-      padding: 0.5rem;
-      text-align: center;
-      text-decoration: none;
-      transition: all 0.5s ease-out;
-    }
 </style>
