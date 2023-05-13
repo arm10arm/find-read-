c650ecbbb91813bd746b465e59a489a7629e6126
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-13 15:17:59 +0700

    Add Backend Validation For Sign-Up

--- backend/route/user.js
+++ backend/route/user.js
@@ -1,5 +1,71 @@
-const express = require("express")
-const pool = require("../config")
+const express = require("express");
+const pool = require("../config");
+const bcrypt = require('bcrypt')
+const Joi = require('joi')
 
 router = express.Router();
 
+const passwordCheck = (value, helpers) => {
+    if (value.length < 8) {
+        throw new Joi.ValidationError('Password must be at least 8 characters long.')
+    }
+    if (!(value.match(/[a-z]/) && value.match(/[A-Z]/) && value.match(/[0-9]/))) {
+        throw new Joi.ValidationError('Password is not strong enough.')
+    }
+    return value
+}
+
+const usernameCheck = async (value, helpers) => {
+    const [rows, _] = await pool.query(
+        "SELECT username FROM users WHERE username = ?",
+        [value]
+    )
+    if (value.length < 8) {
+        throw new Joi.ValidationError('Username must be at least 8 characters long.')
+    }
+    if (rows.length >= 8) {
+        const message = 'This user is already taken'
+        throw new Joi.ValidationError(message, { message })
+    }
+    return value
+}
+
+const signupSchema = Joi.object({
+    email: Joi.string().required().email(),
+    first_name: Joi.string().required().max(150),
+    last_name: Joi.string().required().max(150),
+    password: Joi.string().required().custom(passwordCheck),
+    confirm_password: Joi.string().required().valid(Joi.ref('password')),
+    username: Joi.string().required().min(5).external(usernameCheck),
+})
+
+router.post("/user/signup", async (req, res, next) => {
+    try {
+        await signupSchema.validateAsync(req.body, { abortEarly: false })
+    } catch (err) {
+        return res.status(400).json(err.toString())
+    }
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
+
+    const username = req.body.username
+    const password = await bcrypt.hash(req.body.password, 5)
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+    const email = req.body.email
+
+    try {
+        await conn.query(
+            'INSERT INTO user(username, password, first_name, last_name, email) ' +
+            'VALUES (?, ?, ?, ?, ?)',[username, password, first_name, last_name, email]
+        )
+        conn.commit()
+        res.status(201).send()
+    } catch (err) {
+        conn.rollback()
+        res.status(400).json(err.toString());
+    } finally {
+        conn.release()
+    }
+})
+exports.router = router
