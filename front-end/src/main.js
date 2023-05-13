f0d5727f39996bdb0db501a0ebe0032e0ede75fa
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-13 15:29:10 +0700

    Add Vuelidate

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuelidate from 'vuelidate'
 
 // import './assets/main.css'
 import './assets/tailwind.css'
@@ -8,5 +9,7 @@ import './assets/tailwind.css'
 const app = createApp(App)
 
 app.use(router)
+Vue.use(Vuelidate)
+Vue.config.productionTip = false
 
 app.mount('#app')
