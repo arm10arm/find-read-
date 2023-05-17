d33ee50b6e616361f21f8403e4c3b058d89b8d43
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-17 16:51:28 +0700

    Check Log In Status

---
+++ backend/middlewares/index.js
@@ -0,0 +1,29 @@
+const pool = require("../config");
+async function isLoggedIn(req, res, next) {
+    let authorization = req.headers.authorization
+
+    if (!authorization) {
+        return res.status(401).send('You are not logged in')
+    }
+
+    let [part1, part2] = authorization.split(' ')
+    if (part1 !== 'Bearer' || !part2) {
+        return res.status(401).send('You are not logged in')
+    }
+
+    // Check token
+    const [tokens] = await pool.query('SELECT * FROM tokens WHERE token = ?', [part2])
+    const token = tokens[0]
+    if (!token) {
+        return res.status(401).send('You are not logged in')
+    }
+
+    // Set user
+    const [users] = await pool.query('SELECT user_id, username, first_name, last_name, email, role FROM user WHERE id = ?', 
+    [token.token_user_id])
+    req.user = users[0]
+
+    next()
+}
+
+module.exports = { isLoggedIn }
