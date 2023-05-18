38fadfee78fb99c29d46ae093bd60365aa55d705
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-18 14:29:44 +0700

    Add function

--- front-end/src/views/home.vue
+++ front-end/src/views/home.vue
@@ -1,16 +1,15 @@
-<script setup>
-import navcomp from '../components/navbar.vue'
-import axios from "axios";
-</script>
-
 <template>
+<div>
     <navcomp></navcomp>
     <div class="bg-red-300 w-full flex justify-center items-center" style="height:600px;">
         <div class="h-full bg-slate-400 flex items-center" style="width:400px;">
             <div class="bg-yellow-300 w-full" style="height: 200px;"><br>
                 <p class="text-5xl mt-3">Find-read</p><br>
-                <p class="text-xl">
-                    The more you read the more you get.</p>
+                <p class="text-xl">The more you read the more you get.</p>
+                <div v-if="user">
+                    <p class="text-xl">Welcome, {{ user.username }}</p>
+                    <p>{{ user.role }}</p>
+                </div>
             </div>
         </div>
         <div class="h-full bg-slate-400 flex justify-center items-center" style="width:400px; margin-left: 5em;">
@@ -19,6 +18,38 @@ import axios from "axios";
             </div>
         </div>
     </div>
+    <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" />
+</div>
 </template>
+<script>
+import navcomp from '../components/navbar.vue'
+import axios from '@/plugins/axios'
 
-<style></style>
+export default {
+    name: 'Home',
+    components: {
+        navcomp
+    },
+    data() {
+        return {
+            user: null
+        }
+    },
+    mounted () {
+        this.onAuthChange()
+    },
+    methods: {
+        onAuthChange () {
+            const token = localStorage.getItem('token')
+            if (token) {
+                this.getUser()
+            }
+        },
+        getUser () {
+            axios.get('/user/me').then(res => {
+                this.user = res.data
+            })
+        },
+    }
+    }
+</script>
