864504f3a7b52fe70d82f6a0000cf8e0329a24b7
arm10arm <arm10armarm@gmail.com>
2023-05-18 22:05:47 +0700

    test

---
+++ front-end/src/components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id = "app">
+        <footer class=" w-full bg-slate-900" style="height: 50px;">
+            <p class = "text-center text-slate-200">Find-read</p>
+        </footer>
+    </div>
+</template>
