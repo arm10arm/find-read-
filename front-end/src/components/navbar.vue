78ffd431492938eb8f692225f035f319c4b8cb91
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-18 14:28:17 +0700

    Navbar with worked function

--- front-end/src/components/navbar.vue
+++ front-end/src/components/navbar.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <nav class = "bg-zinc-800 w-auto h-14 flex">
         <!--first nav-items-->
           <div class="flex items-center justify-start h-full w-full ml-5">
@@ -10,27 +11,54 @@
               </span>FIND-READ</span></router-link>
               <router-link to="/catagories"><span  class="ml-5 text-slate-200 hover:text-black">Categories</span></router-link>
               <router-link to="/wishlist"><span  class="ml-5 text-slate-200 hover:text-black">Wishlist</span></router-link>
-              <router-link to="/admin"><span  class="ml-5 text-slate-200 hover:text-black">Admin</span></router-link>
+              <div v-if="role == 'admin'">
+                <router-link to="/admin"><span class="ml-5 text-slate-200 hover:text-black">Admin</span></router-link>
+              </div>
           </div>
         <!------------------->
 
         <!--second-items----->
+          <router-link to="/signup"><button v-if="!user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span  class=" text-slate-200">Sign up</span></button></router-link>
+          <router-link to="/login"><button v-if="!user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span  class=" text-slate-200">Log in</span></button></router-link>
+          <button v-if="user" class="w-24 h-10 rounded-lg bg-zinc-700 my-2 mx-2"><span class=" text-slate-200" @click="logout()">Log out</span></button>
 
-        <div class="flex items-center justify-end h-full w-full mr-5 gap-4">
-          <router-link to="/signup"><button class="w-24 h-10 rounded-lg bg-zinc-700"><span  class=" text-slate-200">Sign up</span></button></router-link>
-          <router-link to="/login"><button class="w-24 h-10 rounded-lg bg-zinc-700"><span  class=" text-slate-200">Log in</span></button></router-link>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-8">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-          </svg>
-
-        </div>
         <!------------------->
-    </nav><br><br>
+    </nav>
+  </div>
 </template>
 <script>
+import axios from '@/plugins/axios'
 export default {
-
+  data() {
+        return {
+            user: null,
+            role: null
+        }
+    },
+    mounted () {
+        this.onAuthChange()
+    },
+    methods: {
+      logout () {
+      localStorage.removeItem('token')
+      this.$emit('auth-change')
+      window.location = '/home'
+    },
+    onAuthChange () {
+      const token = localStorage.getItem('token')
+      if (token) {
+        this.getUser()
+      }
+    },
+    getUser () {
+      axios.get('/user/me').then(res => {
+        this.user = res.data
+        this.role = res.data.role
+      })
+    },
+  }
 }
+
 </script>
 <style>
 
