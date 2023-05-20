6fbd7888abac1b756debec9293c21d34647a8ede
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-20 20:18:42 +0700

    Add Ban and Unban user

--- backend/route/member.js
+++ backend/route/member.js
@@ -3,6 +3,8 @@ const pool = require("../config");
 const { isLoggedIn } = require('../middlewares')
 router = express.Router();
 
+
+// Get Unbanned User Info
 router.get("/members", async function (req, res) {
     const conn = await pool.getConnection()
     // Begin transaction
@@ -24,5 +26,66 @@ router.get("/members", async function (req, res) {
 }
 )
 
-router.put("")
+// Get Banned User Info
+router.get("/banmembers", async function (req, res) {
+    const conn = await pool.getConnection()
+    // Begin transaction
+    await conn.beginTransaction();
+
+    try {
+        const [row] = await pool.query("SELECT * FROM user WHERE ban = 1 AND role = 'user'")
+        console.log(row)
+        await conn.commit()
+        res.json({ user: row })
+    } catch (err) {
+        console.log(err)
+        await conn.rollback();
+        res.json(err)
+    } finally {
+        console.log('finally')
+        conn.release();
+    }
+}
+)
+
+// Ban User
+router.put("/members/:id", async function (req, res) {
+    const conn = await pool.getConnection()
+    // Begin transaction
+    await conn.beginTransaction();
+    try {
+        const [row] = await pool.query("UPDATE user SET ban = 1 WHERE user_id = ?", [req.params.id])
+        console.log(row)
+        await conn.commit()
+        res.json({ message: "Ban success" })
+    } catch (err) {
+        console.log(err)
+        await conn.rollback();
+        res.json(err)
+    } finally {
+        console.log('finally')
+        conn.release();
+    }
+})
+
+// Unban User
+router.put("/banmembers/:id", async function (req, res) {
+    const conn = await pool.getConnection()
+    // Begin transaction
+    await conn.beginTransaction();
+    try {
+        const [row] = await pool.query("UPDATE user SET ban = 0 WHERE user_id = ?", [req.params.id])
+        console.log(row)
+        await conn.commit()
+        res.json({ message: "Unban success" })
+    } catch (err) {
+        console.log(err)
+        await conn.rollback();
+        res.json(err)
+    } finally {
+        console.log('finally')
+        conn.release();
+    }
+})
+
 exports.router = router;
