82c00a95fa9e9bf3ff1d75a90769ac8ca35ab782
arm10arm <arm10armarm@gmail.com>
2023-05-21 13:34:46 +0700

    update authen

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,6 +3,39 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView/>
+  <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" />
 </template>
 
+
+<script>
+import navcomp from '../components/navbar.vue'
+import axios from '@/plugins/axios'
+
+export default {
+    name: 'Home',
+    components: {
+        navcomp
+    },
+    data() {
+        return {
+            user: null
+        }
+    },
+    mounted () {
+        this.onAuthChange()
+    },
+    methods: {
+        onAuthChange () {
+            const token = localStorage.getItem('token')
+            if (token) {
+                this.getUser()
+            }
+        },
+        getUser () {
+            axios.get('/user/me').then(res => {
+                this.user = res.data
+            })
+        },
+    }
+    }
+</script>
--- front-end/src/views/home.vue
+++ front-end/src/views/home.vue
@@ -1,63 +1,37 @@
 <template>
-<div>
-    <navcomp></navcomp>
-    <div class=" w-full flex justify-center items-center" style="height:650px;">
-        <div class="h-full flex items-center" style="width:400px;">
-            <div class=" w-full" style="height: 200px;"><br>
-                <p class="text-5xl mt-3">Find-read</p><br>
-                <p class="text-xl">
-                    The more you read the more you get.</p>
+    <div>
+        <navcomp></navcomp>
+        <div class=" w-full flex justify-center items-center" style="height:650px;">
+            <div class="h-full flex items-center" style="width:400px;">
+                <div class=" w-full" style="height: 200px;"><br>
+                    <p class="text-5xl mt-3">Find-read</p><br>
+                    <p class="text-xl">
+                        The more you read the more you get.</p>
+                </div>
+            </div>
+            <div class="h-full flex justify-center items-center" style="width:500px; margin-left: 5em;">
+                <div style="width: 400px; height: 400px;">
+                    <img src="https://icon-library.com/images/book-icon-transparent/book-icon-transparent-27.jpg"
+                        style="width: 100%; height: 100%;">
+                </div>
             </div>
         </div>
-        <div class="h-full flex justify-center items-center" style="width:500px; margin-left: 5em;">
-            <div  style="width: 400px; height: 400px;">
-                <img src="https://icon-library.com/images/book-icon-transparent/book-icon-transparent-27.jpg" style="width: 100%; height: 100%;">
+        <div class=" w-full flex justify-center items-center bg-zinc-500" style="height:650px;">
+            <div style="width: 600px; height: 400px;">
+                <h1 class="text-5xl text-center text-black underline underline-offset-8 mt-2">Purpose</h1><br>
+                <p class="text-center leading-loose">เว็บนี้เราทําขึ้นมาเพื่อให้ผู้คนผู้ซึ่งอยากจะไปเเค่ดูหนังสือเฉย ๆ
+                    เเต่ไม่อยากซื้อเพราะ
+                    เพราะบางครั้งเวลาที่เราอยากจะดูหนังสือสักเล่มเราก็เเค่สนใจเเต่ไม่ได้ความว่าเราจะ
+                    ซื้อเสมอไปซึ่งเเน่นอนว่าโดยปกติการเข้าร้านหนังสือถ้าเราชินกับการไปที่ไหนบ่อย ๆ เราก็จะนึกถึงที่นั้นเเล้ว
+                    ไปที่นั่นบ่อย ๆ เเต่เเน่นอนว่าพอเราไปบ่อย ๆ เราก็คงจะไม่ชอบกับการที่เราเข้าไปดูหนังสือบ่อยครั้ง
+                    เพราะบางครั้งเราก็อาจจะเกรงใจเขา หรือ ไม่อยากโดนจับตาเวลาที่เราเดินเข้าไป หรือ
+                    เวลาที่เราเข้าเว็บเราอาจจะเข้าเว็บเเต่ว่าเราต้องการเเค่ดูเนื้อหาคร่าว ๆ เเต่ไม่ได้จะซื้อ
+                    ดังนั้น เราจึงทําขึ้นมาเพื่อให้คนที่อยากดูเนื้อหาย่อของหนังสือเท่านั้นโดยที่เขาไม่ได้อยากจะซื้อ เเละ
+                    กลุ่มคนอ่านหนังสือโดยเฉพาะ</p>
             </div>
         </div>
     </div>
-    <div class=" w-full flex justify-center items-center bg-zinc-500" style="height:650px;">
-        <div  style="width: 600px; height: 400px;">
-            <h1 class="text-5xl text-center text-black underline underline-offset-8 mt-2">Purpose</h1><br>
-            <p class= "text-center leading-loose">เว็บนี้เราทําขึ้นมาเพื่อให้ผู้คนผู้ซึ่งอยากจะไปเเค่ดูหนังสือเฉย ๆ เเต่ไม่อยากซื้อเพราะ
-            เพราะบางครั้งเวลาที่เราอยากจะดูหนังสือสักเล่มเราก็เเค่สนใจเเต่ไม่ได้ความว่าเราจะ
-            ซื้อเสมอไปซึ่งเเน่นอนว่าโดยปกติการเข้าร้านหนังสือถ้าเราชินกับการไปที่ไหนบ่อย ๆ เราก็จะนึกถึงที่นั้นเเล้ว
-            ไปที่นั่นบ่อย ๆ เเต่เเน่นอนว่าพอเราไปบ่อย ๆ เราก็คงจะไม่ชอบกับการที่เราเข้าไปดูหนังสือบ่อยครั้ง
-            เพราะบางครั้งเราก็อาจจะเกรงใจเขา หรือ ไม่อยากโดนจับตาเวลาที่เราเดินเข้าไป หรือ เวลาที่เราเข้าเว็บเราอาจจะเข้าเว็บเเต่ว่าเราต้องการเเค่ดูเนื้อหาคร่าว ๆ เเต่ไม่ได้จะซื้อ
-            ดังนั้น เราจึงทําขึ้นมาเพื่อให้คนที่อยากดูเนื้อหาย่อของหนังสือเท่านั้นโดยที่เขาไม่ได้อยากจะซื้อ เเละ กลุ่มคนอ่านหนังสือโดยเฉพาะ</p>
-        </div>
-    </div>
-    <router-view :key="$route.fullPath" @auth-change="onAuthChange" :user="user" />
-</div>
 </template>
 <script>
 import navcomp from '../components/navbar.vue'
-import axios from '@/plugins/axios'
-
-export default {
-    name: 'Home',
-    components: {
-        navcomp
-    },
-    data() {
-        return {
-            user: null
-        }
-    },
-    mounted () {
-        this.onAuthChange()
-    },
-    methods: {
-        onAuthChange () {
-            const token = localStorage.getItem('token')
-            if (token) {
-                this.getUser()
-            }
-        },
-        getUser () {
-            axios.get('/user/me').then(res => {
-                this.user = res.data
-            })
-        },
-    }
-    }
 </script>
