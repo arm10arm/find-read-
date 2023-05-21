d33c494a33b08e29b49d565d3727fc3f0aece738
[Ta1oZ] <[pond.tnd@gmail.com]>
2023-05-21 23:34:41 +0700

    Fix Front End

--- front-end/src/views/add.vue
+++ front-end/src/views/add.vue
@@ -7,8 +7,39 @@ import axios from "axios";
 <template>
     <div id='app'>
         <navcomp></navcomp>
-        <h1 class="text-5xl text-center text-black underline underline-offset-8">Addbook</h1><br><br>
-       
+        <h1 class="text-5xl text-center text-black underline underline-offset-8 py-8">Addbook</h1>
+        <div class="w-full bg-grey-lightest" style="padding-top: 4rem;">
+            <div class="container mx-auto py-8">
+                <div class="w-5/6 lg:w-1/2 mx-auto bg-white rounded shadow">
+                    <div class="py-4 px-8 text-black text-xl border-b border-grey-lighter"><b>Book Info</b></div>
+                    <div class="py-4 px-8">
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="book_name">Book Name</label>
+                            <input class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="book_name" type="text" placeholder="Insert book name here.">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="book_type">Book Type</label>
+                            <input class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="book_type" type="text" placeholder="Insert book name here.">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="author">Book Author</label>
+                            <input class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="author" type="text" placeholder="Insert book author here.">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="publisher">Publisher</label>
+                            <input class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="publisher" type="text" placeholder="Insert publisher here.">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-grey-darker text-sm font-bold mb-2" for="content">Content</label>
+                            <textarea class="appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="content" type="text" placeholder="Insert content here."></textarea>
+                        </div>
+                        <div class="flex items-center justify-between mt-8">
+                            <button class="bg-black text-white font-bold py-2 px-4 rounded-md" type="submit">Update</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <footcomp></footcomp>
     </div>
 </template>
